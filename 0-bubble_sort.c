10f6ab749106eadd110a8e73d97778a7d6a510b1
Diesos <ozturk@live.Fr>
2024-01-16 23:45:12 +0100

    Add bubble sort algorithm and print functions
    This commit adds the bubble sort algorithm implementation in the file "0-bubble_sort.c". It also includes the implementation of the "print_array" and "print_list" functions in the files "print_array.c" and "print_list.c" respectively. These functions are used to print arrays and linked lists of integers.

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/*
+ * bubble_sort - Sorts an array of integers using the Bubble sort algorithm
+ * @array: A pointer to the array of integers to sort
+ * @size: The size of the array
+ *
+ * Return: void
+ */
+
+void	bubble_sort(int *array, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	tmp;
+
+	for (i = 0; i < size; ++i)
+	{
+		for (j = 0; j < (size - 1 - i); ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+	}
+}
