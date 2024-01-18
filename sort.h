6b9819c6b86de3c4607519f20063e5bdefc1c200
MLK59 <abdelmalekmhamed@gmail.com>
2024-01-18 19:29:02 +0100

    Update function prototype t2

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
 #ifndef SORT_H
-# define SORT_H
+#define SORT_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -15,16 +15,18 @@
 
 typedef struct listint_s
 {
-	const int			n;
-	struct listint_s	*prev;
-	struct listint_s	*next;
-}						listint_t;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
-
-void					bubble_sort(int *array, size_t size);
-void					print_array(const int *array, size_t size);
-void					print_list(const listint_t *list);
-void					insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
 
 #endif
