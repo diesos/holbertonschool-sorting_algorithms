93a69376a0530bb4f938c450b17123d278593d2a
MLK59 <abdelmalekmhamed@gmail.com>
2024-01-18 20:03:49 +0100

    version final all prototype

--- sort.h
+++ sort.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -12,7 +12,6 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-
 typedef struct listint_s
 {
 	const int n;
@@ -20,13 +19,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-listint_t *create_listint(const int *array, size_t size);
-
-void bubble_sort(int *array, size_t size);
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
 
 #endif
